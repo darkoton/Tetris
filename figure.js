6669d5955db8dfee698b54a5de56e34429c0324a
Your Name <you@example.com>
2023-08-15 18:25:31 +0300

    add rules for figure S

--- figure.js
+++ figure.js
@@ -37,7 +37,7 @@ class Figure {
             rotateI: this.rotateI,
             rotateJ: this.rotateJ,
             rotateL: this.rotateL,
-            // rotateI: this.rotateI,
+            rotateS: this.rotateS,
             // rotateI: this.rotateI,
 
         }
@@ -338,6 +338,65 @@ class Figure {
             return
         }
     }
+    rotateS(styleCols, styleRows) {
+        if (this.rotate == 1 || this.rotate == 3) {
+            if (this.cells.some(this.checkWallRight)) {
+                styleCols = styleCols.map(el => el - 1)
+            }
+            //COL
+            this.cells[0].style.gridColumn = `${styleCols[0] + 1} / ${styleCols[0] + 2}`
+            this.cells[1].style.gridColumn = `${styleCols[1] + 1} / ${styleCols[1] + 2}`
+            this.cells[2].style.gridColumn = `${styleCols[2]} / ${styleCols[2] + 1}`
+            this.cells[3].style.gridColumn = `${styleCols[3]} / ${styleCols[3] + 1}`
+
+            //ROW
+            this.cells[0].style.gridRow = `${styleRows[0] - 1} / ${styleRows[0]}`
+            this.cells[1].style.gridRow = `${styleRows[1] - 2} / ${styleRows[1] - 1}`
+            this.cells[2].style.gridRow = `${styleRows[2] + 1} / ${styleRows[2] + 2}`
+            this.cells[3].style.gridRow = `${styleRows[3]} / ${styleRows[3] + 1}`
+
+            //POSITION  
+            this.cells[0].dataset.col = styleCols[0] + 1
+            this.cells[1].dataset.col = styleCols[1] + 1
+            this.cells[2].dataset.col = styleCols[2]
+            this.cells[3].dataset.col = styleCols[3]
+
+            this.cells[0].dataset.row = styleRows[0] - 1
+            this.cells[1].dataset.row = styleRows[1] - 2
+            this.cells[2].dataset.row = styleRows[2] + 1
+            this.cells[3].dataset.row = styleRows[3]
+        }
+
+        if (this.rotate == 2 || this.rotate == 4) {
+            //COL
+            this.cells[0].style.gridColumn = `${styleCols[0] - 1} / ${styleCols[0]}`
+            this.cells[1].style.gridColumn = `${styleCols[1] - 1} / ${styleCols[1]}`
+            this.cells[2].style.gridColumn = `${styleCols[2]} / ${styleCols[2] + 1}`
+            this.cells[3].style.gridColumn = `${styleCols[3]} / ${styleCols[3] + 1}`
+
+            //ROW
+            this.cells[0].style.gridRow = `${styleRows[0] + 1} / ${styleRows[0] + 2}`
+            this.cells[1].style.gridRow = `${styleRows[1] + 2} / ${styleRows[1] + 3}`
+            this.cells[2].style.gridRow = `${styleRows[2] - 1} / ${styleRows[2]}`
+            this.cells[3].style.gridRow = `${styleRows[3]} / ${styleRows[3] + 1}`
+
+            //POSITION  
+            this.cells[0].dataset.col = styleCols[0] - 1
+            this.cells[1].dataset.col = styleCols[1] - 1
+            this.cells[2].dataset.col = styleCols[2]
+            this.cells[3].dataset.col = styleCols[3]
+
+            this.cells[0].dataset.row = styleRows[0] + 1
+            this.cells[1].dataset.row = styleRows[1] + 2
+            this.cells[2].dataset.row = styleRows[2] - 1
+            this.cells[3].dataset.row = styleRows[3]
+
+            return
+        }
+    }
+
+
+
     setPosition(styleCols = null, styleRows = null) {
         if (this.type == 'I') {
             //COL
